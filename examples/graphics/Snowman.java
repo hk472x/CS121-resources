7008418027b9c815bd61755b9b9911ad2f3eac56
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2016-01-25 21:28:47 -0700

    Making Mr. Snowman bigger.

--- examples/graphics/Snowman.java
+++ examples/graphics/Snowman.java
@@ -14,8 +14,8 @@ public class Snowman extends JPanel
    	 */
    	public void paintComponent(Graphics page)
 	{
-		final int MID = 150;
-		final int TOP = 50;
+		final int MID = 300;
+		final int TOP = 100;
 
 		page.setColor(Color.CYAN);
 		page.fillRect(0, 0, getWidth(), getHeight());
@@ -23,27 +23,27 @@ public class Snowman extends JPanel
 		Color groundColor = new Color(127, 0, 64);
 
 		page.setColor(groundColor);
-		page.fillRect(0, 175, 300, 50);  // ground
+		page.fillRect(0, 350, 600, 100);  // ground
 
 		page.setColor(Color.yellow);
-		page.fillOval(-40, -40, 80, 80); // sun
+		page.fillOval(-80, -80, 160, 160); // sun
 
 		page.setColor(Color.white);
-		page.fillOval(MID - 20, TOP, 40, 40);      // head
-		page.fillOval(MID - 35, TOP + 35, 70, 50);   // upper torso
-		page.fillOval(MID - 50, TOP + 80, 100, 60);  // lower torso
+		page.fillOval(MID - 40, TOP, 80, 80);      // head
+		page.fillOval(MID - 70, TOP + 70, 140, 100);   // upper torso
+		page.fillOval(MID - 100, TOP + 160, 200, 120);  // lower torso
 
 		page.setColor(Color.black);
-		page.fillOval(MID - 10, TOP + 10, 5, 5);   // left eye
-		page.fillOval(MID + 5, TOP + 10, 5, 5);    // right eye
+		page.fillOval(MID - 20, TOP + 20, 10, 10);   // left eye
+		page.fillOval(MID + 10, TOP + 20, 10, 10);    // right eye
 
-		page.drawArc(MID - 10, TOP + 20, 20, 10, 190, 160);   // smile
+		page.drawArc(MID - 20, TOP + 40, 40, 20, 190, 160);   // smile
 
-		page.drawLine(MID - 25, TOP + 60, MID - 50, TOP + 40);  // left arm
-		page.drawLine(MID + 25, TOP + 60, MID + 55, TOP + 60);  // right arm
+		page.drawLine(MID - 50, TOP + 120, MID - 100, TOP + 80);  // left arm
+		page.drawLine(MID + 50, TOP + 120, MID + 110, TOP + 120);  // right arm
 
-		page.drawLine(MID - 20, TOP + 5, MID + 20, TOP + 5);  // brim of hat
-		page.fillRect(MID - 15, TOP - 20, 30, 25);        // top of hat
+		page.drawLine(MID - 40, TOP + 10, MID + 40, TOP + 10);  // brim of hat
+		page.fillRect(MID - 30, TOP - 40, 60, 50);        // top of hat
    	}
 
 
@@ -52,7 +52,7 @@ public class Snowman extends JPanel
 	 */
 	public Snowman()
 	{
-		setPreferredSize(new Dimension(300, 200));
+		setPreferredSize(new Dimension(600, 400));
 	}
 
 
