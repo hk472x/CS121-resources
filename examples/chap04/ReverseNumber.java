4418b6e6c8a10238aa67cd93020c8a35911c7707
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-02-18 08:33:41 -0700

    Improving comment.

--- examples/chap04/ReverseNumber.java
+++ examples/chap04/ReverseNumber.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 
 /**
- * Demonstrates the use of a do loop.
+ * Demonstrates the use of a do loop by reversing a number. Here
+ * is an example of how the algorithm works.
+ * 
+ * number: 234
+ * 
+ * lastDigit = 234 % 10      = 4
+ * reverse   =  (0 * 10) + 4 = 4
+ * number    = 234 / 10      = 23
+ * 
+ * lastDigit = 23 % 10       = 3
+ * reverse   = (4 * 10) + 3  = 43
+ * number    = 23 / 10       = 2
+ * 
+ * lastDigit =   2 % 10      = 2
+ * reverse   = (43 * 10) + 2 = 432
+ * number    =   2 / 10      = 0 (stop)
+ * 
+ * (Can also watch values using Eclipse debugger)
  * 
  * @author Java Foundations
  */
