46bb5a1bf0129ce299d5d955cd1222b797a6e4a6
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2016-03-30 21:58:48 -0600

    Command-line arg added and minor cleanup

--- examples/chap07/CheckerBoard.java
+++ examples/chap07/CheckerBoard.java
@@ -6,17 +6,24 @@ import javax.swing.JPanel;
 
 /**
  * Demonstrates the use of 2-D arrays to represent a grid.
- * 
- * @author Lewis/Loftus/amit
- * 
+ *
+ * @author CS121 Instructors
+ *
  */
 public class CheckerBoard extends JPanel
 {
+	private static final int MIN_DIM = 4;
+	private int dimension;
+	private Color[][] colors;
+
 	/**
 	 * Sets up the checker board panel.
 	 */
-	public CheckerBoard()
+	public CheckerBoard(int dimension)
 	{
+		this.dimension = dimension;
+		this.colors = getBoardColors();
+
 		setBackground(Color.lightGray);
 		setPreferredSize(new Dimension(500, 500));
 	}
@@ -29,16 +36,13 @@ public class CheckerBoard extends JPanel
 		super.paintComponent(page);
 
 		int x = 0, y = 0;
+
 		int width = getWidth();
 		int height = getHeight();
-		
-		Color[][] colors = getBoardColors();
-		
+
 		int boxWidth = width/colors.length; // assumes it is a square
 		int boxHeight = height/colors.length;
 
-		page.setColor(Color.white);
-		
 		for (int row = 0; row < colors.length; row++)
 		{
 			for (int col = 0; col < colors[row].length; col++)
@@ -51,19 +55,23 @@ public class CheckerBoard extends JPanel
 			y += boxHeight;
 		}
 	}
-	
+
 	public Color[][] getBoardColors()
 	{
-		Color[][] board = new Color[8][8];
+		Color[][] board = new Color[dimension][dimension];
 		for (int row = 0; row < board.length; row++)
 		{
-		   for (int col = 0; col < board[row].length; col++)
-		   {
-		       if(row % 2 == col%2)
-		    	   board[row][col] = Color.red;
-		       else
-		    	   board[row][col] = Color.white;
-		    }
+			for (int col = 0; col < board[row].length; col++)
+			{
+				if(row % 2 == col % 2)
+				{
+					board[row][col] = Color.RED;
+				}
+				else
+				{
+					board[row][col] = Color.BLACK;
+				}
+			}
 		}
 		return board;
 	}
@@ -74,9 +82,31 @@ public class CheckerBoard extends JPanel
 	 */
 	public static void main(String[] args)
 	{
+		if(args.length != 1)
+		{
+			System.out.println("Usage: CheckerBoard <dimension>");
+			System.exit(1);
+		}
+
+		int dimension = 0;
+		try
+		{
+			dimension = Integer.parseInt(args[0]);
+			if(dimension < CheckerBoard.MIN_DIM)
+			{
+				System.out.println("Dimension must be larger than " + CheckerBoard.MIN_DIM);
+				System.exit(1);
+			}
+		}
+		catch (NumberFormatException e)
+		{
+			System.out.println("Invalid dimension: " + args[0]);
+			System.exit(1);
+		}
+
 		JFrame frame = new JFrame("Board");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		CheckerBoard panel = new CheckerBoard();
+		CheckerBoard panel = new CheckerBoard(dimension);
 		frame.getContentPane().add(panel);
 		frame.pack();
 		frame.setVisible(true);
