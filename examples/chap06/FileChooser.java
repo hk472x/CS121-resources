1536d76b3b83e43223661c03277226923d58d26f
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-04-12 15:52:58 -0600

    Cleaned up JFileChooser example.

--- examples/chap06/FileChooser.java
+++ examples/chap06/FileChooser.java
@@ -1,59 +1,86 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
-
 /**
   Demonstrates the use of a file chooser and a text area.
-  @author Lewis and Loftus.
-  @author amit
+  @author CS121 Instructors
  */
-
-
-
-public class FileChooser
+@SuppressWarnings("serial")
+public class FileChooser extends JPanel
 {
+	private JTextArea filePreview;
+	private JButton openFileButton;
+	
 	/**
 	 * Opens a file chooser dialog, reads the selected file and loads it into a
 	 * text area.
 	 */
+	public FileChooser()
+	{
+		openFileButton = new JButton("Select File");
+		openFileButton.addActionListener(new ButtonActionListener());
+		
+		filePreview = new JTextArea(20, 30);
+		JScrollPane scrollPane = new JScrollPane(filePreview,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		add(scrollPane);
+		add(openFileButton);
+	}
+	
+	private class ButtonActionListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent event)
+		{
+			// The following starts in the home folder
+			//JFileChooser chooser = new JFileChooser();
+			
+			// The following starts in the current folder, which is often convenient
+			JFileChooser chooser = new JFileChooser(".");
+			
+			int status = chooser.showOpenDialog(null);
+
+			if (status != JFileChooser.APPROVE_OPTION) {
+				filePreview.setText("No File Chosen");
+			} else {
+				File file = chooser.getSelectedFile();
+				
+				// Read the contents of the file and display.
+				// We don't have to read the contents, we could just pass the
+				// File along.
+				try {
+					Scanner scan = new Scanner(file);
+					while (scan.hasNext()) {
+						String line = scan.nextLine() + "\n";
+						filePreview.append(line);
+					}
+					scan.close();
+				} catch(FileNotFoundException e) {
+					filePreview.setText("Could not open file: " + file.getPath());
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) throws IOException
 	{
 		JFrame frame = new JFrame("Display File");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JTextArea ta = new JTextArea(20, 30);
-		JScrollPane scrollpane = new JScrollPane(ta,
-				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		// The following starts in the home folder
-		//JFileChooser chooser = new JFileChooser();
-
-		// The following starts in the current folder, which is often convenient
-		JFileChooser chooser = new JFileChooser(".");
-		
-		int status = chooser.showOpenDialog(null);
-
-		if (status != JFileChooser.APPROVE_OPTION)
-			ta.setText("No File Chosen");
-		else {
-			File file = chooser.getSelectedFile();
-			Scanner scan = new Scanner(file);
-
-			String info = "";
-			while (scan.hasNext())
-				info += scan.nextLine() + "\n";
-
-			ta.setText(info);
-		}
-
-		frame.getContentPane().add(scrollpane);
+		frame.getContentPane().add(new FileChooser());
 		frame.pack();
 		frame.setVisible(true);
 	}
